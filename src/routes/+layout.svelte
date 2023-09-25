0c30349182b59e12268f625e7de4ebde13e9433c
Leo <thcheetah777@gmail.com>
2023-09-25 07:28:13 +0300

    set up supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
+  import type { LayoutData } from "./$types";
+  import { onMount } from "svelte";
+  import { invalidate } from "$app/navigation";
+
   import "../global.css";
+
+  export let data: LayoutData;
+
+  let { supabase, session } = data;
+  $: ({ supabase, session } = data);
+
+  onMount(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, _session) => {
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth");
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  });
 </script>
 
 <svelte:head>
